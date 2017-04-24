968c138d9f42e3e085c5e9a654724e694386ef6b
Valera Tutaev <valeratutaev@gmail.com>
2017-04-25 02:16:04 +0300

    update provide settiing - auth working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret']
+  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'],  callback_url: 'http://localhost:3000/'
 end
